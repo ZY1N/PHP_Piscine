449729c79771c2738ee1307744b8698d54c020ce
ZY1N <zhang.yine@husky.neu.edu>
2020-02-25 17:34:14 -0800

    abc

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -1,4 +1,14 @@
 #! /usr/bin/php
 <?php
-
+function upper($matches){
+    return ($matches[1].$matches[2].strtoupper($matches[3]).$matches[4].strtoupper($matches[5]).$matches[6]);}
+$tab = file($argv[1]);
+$string = "";
+foreach ($tab as $line)
+{
+    if(preg_match("/<a/", $line) > 0)
+        $line = preg_replace_callback('/(<a.*)(title=")(.*)(">)(.*)(<\/a>)/', 'upper' , $line);
+    $string .= $line;
+}
+echo $string;
 ?>
