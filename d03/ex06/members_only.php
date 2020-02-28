aafe267e427da0528c9b1303250cae2205aaadfe
ZY1N <zhang.yine@husky.neu.edu>
2020-02-27 20:16:18 -0800

    abc

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER['PHP_AUTH_USER'] == 'zaz' && $_SERVER['PHP_AUTH_PW'] == 'jaimelespetitsponeys')
 {
-    $base64pic = base64_encode(file_get_contents("./img/42.png"));
+    $base64pic = base64_encode(file_get_contents("../img/42.png"));
 	echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,".$base64pic."'>\n</body></html>\n";
 }
 else
