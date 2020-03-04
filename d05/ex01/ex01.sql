2a038226a9114a4a53451a6fa274ac765d362fd4
ZY1N <zhang.yine@husky.neu.edu>
2020-03-03 20:42:45 -0800

    abc

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS ft_table (
     `id` INT NOT NULL AUTO_INCREMENT,
     `login` VARCHAR(255) NOT NULL DEFAULT 'toto',
     `group` ENUM('staff', 'student', 'other') NOT NULL,
+
     `creation_date` DATE NOT NULL,
     PRIMARY KEY(id)
 );
